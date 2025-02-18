5c046dccb5179843325aa31548ed727bc7670e7c
viraj159 <virajchoksi276@gmail.com>
2025-02-17 18:31:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ See assignment-01.pdf for details.
 # no imports needed.
 
 def foo(x):
-    ### TODO
-    pass
+    if x <= 1:
+        return x
+    else:
+        ra = foo(x - 1)
+        rb = foo(x - 2)
+        return ra + rb
+    
 
 def longest_run(mylist, key):
     ### TODO
